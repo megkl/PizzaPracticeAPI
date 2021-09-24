c54e93eb22f2b7a7cfa9fc029d44647af9601129
meg <maggiekendra12@gmail.com>
2021-09-24 15:07:35 +0300

    Creation of service class

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -1,12 +1,84 @@
-﻿using System;
+﻿using PizzaPracticeAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PizzaPracticeAPI.Services
 {
-    public class PizzaService
+    public static class PizzaService
     {
+        static List<Pizza> Pizzas { get; }
+        static int nextId = 3;
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>
+           {
+               new Pizza{Id = 1, PizzaName = "Hawaaian", IsGlutenFree = true},
+               new Pizza { Id = 2, PizzaName = "BBQ Steak", IsGlutenFree = false},
+               new Pizza {Id = 3, PizzaName = "Borewores", IsGlutenFree = false}
+           };
+        }
 
+        public static List<Pizza> GetAll()
+        {
+            return Pizzas;
+        }
+
+        public static Pizza GetPizza(int id)
+        {
+            return Pizzas.FirstOrDefault(x => x.Id == id);
+        }
+
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+            if (index == -1)
+                return;
+
+            Pizzas[index] = pizza;
+        }
+
+        public static void Delete(int id)
+        {
+            var pizza = GetPizza(id);
+            if (pizza is null)
+                return;
+
+            Pizzas.Remove(pizza);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
