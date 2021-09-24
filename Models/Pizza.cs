da6e81f5b59518eb4a1312f4337a66cf7f4f37b3
meg <maggiekendra12@gmail.com>
2021-09-24 14:39:20 +0300

    Add project files.

---
+++ Models/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPracticeAPI.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        public String PizzaName { get; set; }
+
+        public bool IsGlutenFree { get; set; }
+    }
+}
